804ac4e0f5f2d1e18c5678596a3035f65ff6c152
Orelion <orelionhighborn@icloud.com>
2025-02-23 20:13:58 -0800

    initial commit, all tests successfully passed.

---
+++ src/main/java/edu/sdccd/cisc191/Option.java
@@ -0,0 +1,14 @@
+package edu.sdccd.cisc191;
+
+public class Option {
+    private final String details;
+
+    public Option(String details) {
+        this.details = details;
+    }
+
+
+    public String getDetails() {
+        return details;
+    }
+}
---
+++ src/main/java/edu/sdccd/cisc191/PickupTruck.java
@@ -0,0 +1,16 @@
+package edu.sdccd.cisc191;
+
+public class PickupTruck extends Vehicle {
+
+    private final int cargoCapacity;
+
+
+    public PickupTruck(String manufacturerName, int milesOnVehicle, int price, int numberOfSeats, Option[] options, int cargoCapacity) {
+        super(manufacturerName, milesOnVehicle, price, numberOfSeats, options);
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+}
